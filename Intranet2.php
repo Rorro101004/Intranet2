7056c25570be99eb487b7acbbbdda6f44e439c80
Rorro101004 <rodrigofernando.cg@gmail.com>
2025-05-14 21:07:25 +0200

    commit

--- Intranet2.php
+++ Intranet2.php
@@ -15,94 +15,6 @@
         <h1>Bienvenido a la intranet de RodBi Technology</h1>
     </header>
     <section>
-
-
-        <?php
-        $meses = [
-            "Enero",
-            "Febrero",
-            "Marzo",
-            "Abril",
-            "Mayo",
-            "Junio",
-            "Julio",
-            "Agosto",
-            "Septiembre",
-            "Octubre",
-            "Noviembre",
-            "Diciembre"
-        ];
-        $dia_actual = date("d");
-        $mes_actual = date("m");
-        $anio_actual = date("Y");
-
-        ?>
-        <div id="calendario">
-            <h2 style="display: inline;">Calendario del mes en curso:</h2>
-            <?php
-            echo "<h2 class='mes'>" . $meses[$mes_actual - 1] . "</h2>";
-            ?>
-
-            <?php
-            $meses = [
-                "Enero",
-                "Febrero",
-                "Marzo",
-                "Abril",
-                "Mayo",
-                "Junio",
-                "Julio",
-                "Agosto",
-                "Septiembre",
-                "Octubre",
-                "Noviembre",
-                "Diciembre"
-            ];
-            $dia_actual = date("d");
-            $mes_actual = date("m");
-            $anio_actual = date("Y");
-
-            ?>
-
-            <table>
-                <tr>
-                    <th>Lunes</th>
-                    <th>Martes</th>
-                    <th>Miércoles</th>
-                    <th>Jueves</th>
-                    <th>Viernes</th>
-                    <th>Sábado</th>
-                    <th>Domingo</th>
-                </tr>
-                <?php
-                $primer_dia_mes = strtotime("$anio_actual-$mes_actual-01");
-                echo "<br>";
-                $dia_inicio = date("N", $primer_dia_mes);
-                $total_dias = date("t", $primer_dia_mes);
-
-
-
-                echo "<tr>";
-                for ($i = 1; $i < $dia_inicio; $i++) {
-
-                    echo "<td></td>";
-                }
-
-                for ($dia = 1; $dia <= $total_dias; $dia++) {
-                    if ($dia == $dia_actual) {
-                        echo "<td style=' border: 3px dashed black; border-radius: 50%; background-color:rgb(46, 113, 172);'>$dia</td>";
-                    } else {
-                        echo "<td>$dia</td>";
-                    }
-                    if (($dia + $dia_inicio - 1) % 7 == 0) {
-                        echo "</tr><tr>";
-                    }
-                }
-                echo "</tr>";
-                ?>
-            </table>
-        </div>
-
         <div id="forms">
             <form method="post" class="type">
                 <input type="submit" name="rol" value="Profesor">
@@ -137,12 +49,12 @@
                 } elseif ($rol == "Profesor") {
                     echo "<a href='https://web2.alexiaedu.com/ACWeb/LogOn.aspx?key=%2fNYuvQedqk4%3d' target='_blank'>Ir a la página de Profesor</a>";                    
                     exit();
-                } else {
-                    echo "<h2>Error: Por favor, seleccione un rol antes de iniciar sesión.</h2>";
-                }
+                } 
             }
         }
-
+        if (!isset($_SESSION['rol'])) {
+        echo "<h2>Seleccione un rol antes de iniciar sesión.</h2>";
+}
         ?>
     </section>
     <footer>
